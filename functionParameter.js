3070139e5c8a98c90cab99d43239d5633a5f1a28
hyuntae384 <hyuntae384@gmail.com>
2022-11-10 23:46:00 +0900

    first

---
+++ functionParameter.js
@@ -0,0 +1,11 @@
+function functionParameter(func, num) {
+  // TODO: 여기에 코드를 작성합니다.
+  return func(num);
+}
+
+/*function division2(num){
+  return num / 2;
+}
+
+let result = functionParameter(division2,4);
+console.log(result);*/
